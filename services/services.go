780925fcef562070bf9a75401ae628b106eb04ad
Roshan Mehta <sonicroshan122@gmail.com>
2020-04-12 18:54:35 +0530

    Added urls to three new services

--- services/services.go
+++ services/services.go
@@ -1,21 +1,27 @@
 package services
 
 const (
-	//UsersService is used to name and locate users service
-	UsersService = "users-srv"
-	//JWTService is used to name and locate jwt service
+	// UserMainService is used to name and locate user main service
+	UserMainService = "user-main-srv"
+	// UserExtraService is used to name and locate user extra service
+	UserExtraService = "user-extra-srv"
+	// UserMetaService is used to name and locate user meta service
+	UserMetaService = "user-meta-srv"
+	// JWTService is used to name and locate jwt service
 	JWTService = "jwt-srv"
-	//EmailVerificationSrv is used to name and locate email verification service
+	// EmailVerificationSrv is used to name and locate email verification service
 	EmailVerificationSrv = "email-verification-srv"
-	//AuthServerService is used to name and locate auth server service
+	// AuthServerService is used to name and locate auth server service
 	AuthServerService = "auth-server-srv"
-	//ResourceServerService is used to name and locate resource server service
+	// ResourceServerService is used to name and locate resource server service
 	ResourceServerService = "resource-server-srv"
 )
 
-//ServiceUrls is a map of all services and their url
+// ServiceUrls is a map of all services and their url
 var ServiceUrls = map[string]string{
-	UsersService:         "127.0.0.1:3000",
+	UserMainService:      "127.0.0.1:3000",
 	JWTService:           "127.0.0.1:3001",
 	EmailVerificationSrv: "127.0.0.1:3002",
+	UserExtraService:     "127.0.0.1:3003",
+	UserMetaService:      "127.0.0.1:3004",
 }
